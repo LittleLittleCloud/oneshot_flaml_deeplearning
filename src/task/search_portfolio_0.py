5d838d742612b5a73ee97b4946a828aaa2c63fe9
xiaoyun <g2260578356@gmail.com>
2023-01-31 17:28:09 +0000

    update search_portfolio

--- src/task/search_portfolio_0.py
+++ src/task/search_portfolio_0.py
@@ -4,7 +4,16 @@ import torch
 from torch.utils.data import DataLoader, Subset, RandomSampler, SubsetRandomSampler
 from src.task.task_helper import *
 import json
+import argparse
 if __name__ == '__main__':
-    device = torch.device('cuda:0')
-    search_portfolio(DATASET_GROUP_0, device)
-    evaluate_portfolio(DATASET_GROUP_0, device)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--device', default=0, type = int )
+    parser.add_argument('--data', type = str )
+    args = parser.parse_args()
+    data = args.data
+    dataset_group = DATASET_GROUP_0 + DATASET_GROUP_2
+    dataset_group = filter(lambda x: data in x.__name__, dataset_group)
+
+    device = torch.device(f'cuda:{args.device}')
+    search_portfolio(dataset_group, device)
+    evaluate_portfolio(dataset_group, device)
