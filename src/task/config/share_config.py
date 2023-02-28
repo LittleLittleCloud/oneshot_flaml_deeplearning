f40ab0180611084cecfcf29ce360b82a2eaf403b
LittleLittleCloud <g2260578356@gmail.com>
2023-02-28 09:28:02 +0000

    update model

---
+++ src/task/config/share_config.py
@@ -0,0 +1,40 @@
+from src.dataset import *
+
+SHARE_CONFIG = {
+    "M": 100, # m step
+    "EPOCH": 100,
+    "Train": [
+        load_DTD,
+        load_caltech101,
+        load_cifar10,
+        load_EMNIST,
+        load_EuroSAT,
+        load_FGVCAircraft,
+        load_GTSRB,
+        load_Places365,
+        load_StanfordCars,
+        load_Food101
+    ],
+    "Validate": [
+        load_SEMEION,
+        load_PCAM,
+        load_STL10,
+        load_caltech256,
+        load_cifar100,
+        load_celebA,
+        load_country211,
+        load_FashionMNIST,
+        load_Flowers102,
+        load_KMNIST,
+        load_Omniglot,
+        load_OxfordIIITPet,
+        load_QMNIST,
+        load_RenderedSST2
+    ],
+    "SUB_SAMPLE": -1, 
+    "SEED": 12357
+}
+
+BLEND_SEARCH_CONFIG = {
+    **SHARE_CONFIG
+}
